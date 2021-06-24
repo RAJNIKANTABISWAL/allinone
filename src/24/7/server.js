5afc76c2007804eb038d3e06c613d90cfdee8ed5
CRAB <83424761+RAJNIKANTABISWAL@users.noreply.github.com>
2021-06-24 12:35:55 +0530

    Add files via upload

---
+++ src/24/7/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => res.send('TADASHI CODE 24/7 ALL Copyright Gots TO: Khyren'))
+app.listen(port, () => console.log(`Need Help? Content us Here: https://discord.gg/pmF7hue4PA`))
---
+++ src/events/ready.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+const db = require('quick.db');
+
+module.exports = async(client) => {
+    console.log(chalk.yellow(figlet.textSync('NSystem', { horizontalLayout: 'full' })));
+    console.log(chalk.red(`Bot started!
+=====================================
+> Users: ${client.users.cache.size}
+> Channels: ${client.channels.cache.size}
+> Servers: ${client.guilds.cache.size}
+> Bot Ping: ${client.ws.ping} XD
+> Prefix: ${client.config.bot.setting.main_prefix}
+> Developer: @Khyren
+#7558
+> Support: https://discord.gg/pmF7hue4PA
+=====================================`))
+
+    client.user.setActivity(client.config.bot.setting.main_prefix + "help");
+}
